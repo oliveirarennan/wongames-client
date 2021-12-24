4a4bd764830ffd590725dc321c99e2f49e5e66b0
Rennan Neves de Oliveira <rennan_neves@live.com>
2021-12-24 11:47:32 -0300

    feat: add Button Component

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, Story } from '@storybook/react'
+import { AddShoppingCart } from '@styled-icons/material/AddShoppingCart'
+import Button from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  },
+  args: {}
+} as Meta
+
+export const Default: Story = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy now'
+}
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />
+}
+
+export const withClickEvent: Story = (args) => <Button {...args} />
+
+withClickEvent.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />,
+  onclick: () => alert('Click Event dispatch')
+}
