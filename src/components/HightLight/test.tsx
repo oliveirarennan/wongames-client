d3f09a654ec35206bffa0badf99824c1cfb03dfc
Rennan Neves de Oliveira <rennan_neves@live.com>
2021-12-28 13:13:35 -0300

    feat: add Highlight Component

---
+++ src/components/HightLight/test.tsx
@@ -0,0 +1,72 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import HightLight from '.'
+import * as S from './styles'
+
+const props = {
+  title: 'Heading 1',
+  subtitle: 'Heading 2',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2',
+  backgroundImage: '/img/red-dead-img-.jpg'
+}
+
+describe('<HightLight />', () => {
+  it('should render the headings and button', () => {
+    renderWithTheme(<HightLight {...props} />)
+    expect(
+      screen.getByRole('heading', { name: /heading 1/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /heading 2/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+  })
+
+  it('should render background image', () => {
+    const { container } = renderWithTheme(<HightLight {...props} />)
+    expect(container.firstChild).toHaveStyle({
+      backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+  it('should render float image', () => {
+    renderWithTheme(<HightLight {...props} floatImage="/float-image.png" />)
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
+  })
+
+  it('should render align right by default', () => {
+    const { container } = renderWithTheme(<HightLight {...props} />)
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatImage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should render align left', () => {
+    const { container } = renderWithTheme(
+      <HightLight alignment="left" {...props} />
+    )
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatImage'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should match with Snapshot', () => {
+    const { container } = renderWithTheme(<HightLight {...props} />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
