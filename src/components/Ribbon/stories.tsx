7c9473287c7c51effa33cd2122edba8be658e78d
Rennan Neves de Oliveira <rennan_neves@live.com>
2021-12-28 10:57:54 -0300

    feat: add Riboon

---
+++ src/components/Ribbon/stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, Story } from '@storybook/react'
+import Ribbon, { RibbonProps } from '.'
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon,
+  args: {
+    children: 'Best Seller'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<RibbonProps> = (args) => (
+  <div
+    style={{
+      width: '40rem',
+      height: '25rem',
+      position: 'relative',
+      backgroundColor: '#888'
+    }}
+  >
+    <Ribbon {...args} />
+  </div>
+)
