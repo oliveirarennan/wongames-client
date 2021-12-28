236cbd47389c7e64f2b70ebb5c19a3e29ab77bef
Rennan Neves de Oliveira <rennan_neves@live.com>
2021-12-28 11:34:59 -0300

    feat: add Ribbon to Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,20 +1,33 @@
 import Button from 'components/Button'
+import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 import * as S from './styles'
+
 export type BannerProps = {
   image: string
   title: string
   subtitle: string
   buttonLabel: string
   buttonLink: string
+  ribbon?: string
+  ribbonSize?: RibbonSizes
+  ribbonColor?: RibbonColors
 }
 const Banner = ({
   image,
   title,
   subtitle,
   buttonLabel,
-  buttonLink
+  buttonLink,
+  ribbon,
+  ribbonColor,
+  ribbonSize
 }: BannerProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
     <S.Image src={image} role="image" aria-label={title} />
     <S.Caption>
       <S.Title>{title}</S.Title>
